f9dfa8e9aa0b00d1e2ca0d7a750d2ba4735bc163
sanasdh <sanasdh@gmail.com>
2020-09-04 08:19:39 -0400

    first commit

---
+++ Count_Me_In/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Count_Me_In project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Count_Me_In.settings')
+
+application = get_asgi_application()
