62c339c9d023f031f2a37c530af7f4a206199192
sanasdh <sanasdh@gmail.com>
2020-09-08 13:46:42 -0400

    plan

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from .models import Plans
 
 
 def plans_index(request):
-    return render(request, "plan/index.html")
+    plans = Plans.objects.all()
+    return render(request, "plan/index.html", {"plans": plans})
+
+
+def plans_detail(request, plan_id):
+    plan = Plans.objects.get(id=paln_id)
+    return render(request, "plan/detail.html", {"plan": plan})
+
+
+def plans_create(request):
+    return render(request, "plan/create.html")
