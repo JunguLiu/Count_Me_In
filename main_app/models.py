1d3a5cdc94e6300249b8994addda94a90a5c98d2
sanasdh <sanasdh@gmail.com>
2020-09-08 12:05:30 -0400

    admin-databse

--- main_app/models.py
+++ main_app/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-# Create your models here.
 
 
 class Workouts(models.Model):
-    name = models.CharField(max_length=100),
+    name = models.CharField(max_length=100)
+    calories = models.IntegerField()
+    category = models.CharField(max_length=100)
+    image = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Comments(models.Model):
-    name = models.CharField(max_length=100),
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Plans(models.Model):
     name = models.CharField(max_length=100),
-    workouts = models.ManyToManyField(Workouts),
-    comments = models.ForeignKey(Comments, on_delete=models.CASCADE),
+    workouts = models.ManyToManyField(Workouts)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
     date = models.DateField
 
     def __str__(self):
@@ -23,3 +31,14 @@ class Plans(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'plan_id': self.id})
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.IntegerField()
+    goalWeight = models.IntegerField()
+    age = models.IntegerField()
+    plans = models.ForeignKey(Plans, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
