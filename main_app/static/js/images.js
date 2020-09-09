124df6cdd41dc19432e6f41da959eafbb9065442
Frank-liu <youquan.liu@hotmail.com>
2020-09-09 17:57:28 -0400

    js folder

---
+++ main_app/static/js/images.js
@@ -0,0 +1,18 @@
+$('.itemBox').on('click', function () {
+    $(this).toggleClass('show-description');
+});
+
+$(document).ready(function () {
+    $('.list').click(function () {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.itemBox').show('1000');
+        } else {
+            $('.itemBox').not('.' + value).hide('1000');
+            $('.itemBox').filter('.' + value).show('1000');
+        }
+    })
+    $('.list').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+})
