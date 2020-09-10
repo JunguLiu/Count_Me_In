350cc54f116074d4f833b00da8f5819f410f2b5c
Frank-liu <youquan.liu@hotmail.com>
2020-09-10 18:53:16 -0400

    change route to main page after login and register

--- Count_Me_In/settings.py
+++ Count_Me_In/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/plans/'
+LOGIN_REDIRECT_URL = '/'
 
 # Add this variable to specify where logging out redirects to
-LOGOUT_REDIRECT_URL = '/plans'
+LOGOUT_REDIRECT_URL = '/'
